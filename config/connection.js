1639d286c61097ce34a64c1aca4616e2c9255ead
erika helm <erikadphelm16@gmail.com>
2023-01-30 01:31:30 -0600

    getting there...

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
-const { mongoose } = require('mongoose')
+// const { mongoose } = require('mongoose')
 
-const DATABASE_URL = 'mongodb://127.0.0.1:27020/mongoose-relationships'
+// const DATABASE_URL = 'mongodb://127.0.0.1:27020/mongoose-relationships'
 
-const CONFIG = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-}
-mongoose.set('strictQuery', false)
-mongoose.connect(DATABASE_URL, CONFIG)
+// const CONFIG = {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// }
+// mongoose.set('strictQuery', false)
+// mongoose.connect(DATABASE_URL, CONFIG)
 
-mongoose.connection
-	.on('open', () => console.log('Connected to Mongoose'))
-	.on('close', () => console.log('Disconnected from Mongoose'))
-	.on('error', (error) => console.log(error))
+// mongoose.connection
+// 	.on('open', () => console.log('Connected to Mongoose'))
+// 	.on('close', () => console.log('Disconnected from Mongoose'))
+// 	.on('error', (error) => console.log(error))
 
-module.exports = mongoose
+// module.exports = mongoose
